30ada130b37d3c15240f82cbb9365135251976a1
eraera <126274607+Ataulla56@users.noreply.github.com>
2025-07-22 05:05:15 +0600

    Update swap.js

--- api/swap.js
+++ api/swap.js
@@ -1,14 +1,15 @@
-
 // api/swap.js
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
     const { fromToken, toToken, amount } = req.body;
 
-    // You can add actual swap logic here or call another service
-    console.log("Swap requested:", { fromToken, toToken, amount });
+    console.log("Received swap request:", { fromToken, toToken, amount });
 
-    res.status(200).json({ message: 'Swap successful', data: { fromToken, toToken, amount } });
+    res.status(200).json({
+      message: 'Swap successful',
+      data: { fromToken, toToken, amount }
+    });
   } else {
     res.status(405).json({ error: 'Method Not Allowed' });
   }
